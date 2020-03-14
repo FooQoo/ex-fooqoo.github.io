80d6ad8f0b8d026b1963b7b439370f1dddf1f5ba
FooQoo <horseshoes.buzz.17@gmail.com>
2020-03-15 01:43:13 +0900

    update UI

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,23 @@
 module.exports = {
-  title: 'FooQooのメモ',
-  description: '普段気になったことについて'
+    title: 'FooQooのメモ',
+    description: '普段気になったことについて',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: '開発', link: '/dev/' },
+
+        ],
+        sidebar: [
+            '/',
+            {
+                title: '開発',
+                collapsable: true,
+                children: [
+                    '/dev/vuepress.html'
+                ],
+                path: '/dev/'
+            }
+       ]
+    },
+    displayAllHeaders: true
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,6 @@
-# Hello VuePress
+# FooQooのメモ
+日常生活で気になったことについて書きます。
+
+## SNS
+- [twitter](https://twitter.com/FooQoo56)
+- [github](https://github.com/FooQoo)
